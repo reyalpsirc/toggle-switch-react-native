1863586ad324cfe4338132baedfe3afb1a8e614b
Cristiano Santos <cristianomnsantos@gmail.com>
2018-01-09 17:23:55 +0000

    - Added the properties borderTrackOnColor, borderTrackOffColor, circleOnColor, circleOffColor, trackStyle, width, height, circleWidth, circleHeight, onImageSource, offImageSource. - Removed some code so that this is kept as dynamic as possible

--- ToggleSwitch.js
+++ ToggleSwitch.js
@@ -15,7 +15,8 @@ import {
   TouchableOpacity,
   Image,
   Animated,
-  Easing
+  Easing,
+  ViewPropTypes
 } from 'react-native'
 
 import PropTypes from 'prop-types'
@@ -28,19 +29,28 @@ class ToggleSwitch extends React.Component{
         this.state = {
             isOn: this.props.isOn,
             label: this.props.label,
-            offsetX: new Animated.Value(0),
-            initialDirection:  (this.props.isOn) ? {right: 0} : {left: 0},
+            animVal: new Animated.Value(this.props.isOn ? 1 : 0)
         }
-        this.calculateDimensions(this)
     }
 
     static propTypes = {
         isOn: PropTypes.bool.isRequired,
         label: PropTypes.string,
-        onColor: PropTypes.string.isRequired,
-        offColor: PropTypes.string.isRequired,
+        onColor: PropTypes.string,
+        offColor: PropTypes.string,
+        borderTrackOnColor: PropTypes.string,
+        borderTrackOffColor: PropTypes.string,
+        circleOnColor: PropTypes.string,
+        circleOffColor: PropTypes.string,
+        trackStyle: ViewPropTypes.style,
         size: PropTypes.string,
         labelStyle: PropTypes.object,
+        width: PropTypes.number,
+        height: PropTypes.number,
+        circleWidth: PropTypes.number,
+        circleHeight: PropTypes.number,
+        onImageSource: Image.propTypes.source,
+        offImageSource: Image.propTypes.source,
         onToggle: PropTypes.func.isRequired
     }
 
@@ -48,40 +58,30 @@ class ToggleSwitch extends React.Component{
         isOn : false,
         onColor: '#634fc9',
         offColor: '#ecf0f1',
-        size: 'medium',
+        borderTrackOnColor: 'transparent',
+        borderTrackOffColor: 'transparent',
+        circleOnColor: 'green',
+        circleOffColor: 'green',
+        width: 60,
+        height: 24,
+        circleWidth: 18,
+        circleHeight: 18,
+        onImageSource: null,
+        offImageSource: null,
         labelStyle: {}
     }
 
-    calculateDimensions(){
-        let dimensions = {}
-        switch(this.props.size){
-            case 'small':
-                dimensions = {width: 50, padding: 10, cercleWidth: 15, cercleHeight: 15, translateX: 22}
-            break;
-            case 'large':
-                dimensions = {width: 100, padding: 20, cercleWidth: 30, cercleHeight: 30, translateX: 38}
-            break;
-            default:
-                dimensions = {width: 60, padding: 12, cercleWidth: 18, cercleHeight: 18, translateX: 26}
-            break;
-        }
-        this.state.dimensions = dimensions
-    }
-    
     onToggle(){
-        
-        if(this.props.isOn)
-            toValue = this.state.isOn ?  (- this.state.dimensions.width + (this.state.dimensions.translateX)  ) : 0
-        else
-            toValue = this.state.isOn ?  0 : ( this.state.dimensions.width - (this.state.dimensions.translateX) )
+        toValue = this.state.isOn ? 0 :1
         Animated.timing(
-            this.state.offsetX,
+            this.state.animVal,
             {
               toValue: toValue,
+              useNativeDriver: true,
               duration: 300,
-            }                              
+            }
         ).start();
-        
+
         let newState = !this.state.isOn
 
         this.setState({
@@ -97,8 +97,67 @@ class ToggleSwitch extends React.Component{
         return (
             <View style={styles.container}>
                 { (this.props.label) ? <Text style={[styles.labelStyle, this.props.labelStyle]}>{this.props.label}</Text> : null}
-                <TouchableOpacity style={{ justifyContent: 'center', width: this.state.dimensions.width, borderRadius: 20, padding: this.state.dimensions.padding, backgroundColor: (this.state.isOn) ? this.props.onColor : this.props.offColor}} activeOpacity={0.8} onPress={this.onToggle.bind(this)}>
-                    <Animated.View style={{  margin: 4, transform: [{translateX: this.state.offsetX}] ,position: 'absolute', ...this.state.initialDirection , width: this.state.dimensions.cercleWidth, height: this.state.dimensions.cercleHeight, borderRadius: (this.state.dimensions.cercleWidth/2), backgroundColor: 'white'}} />
+                <TouchableOpacity onPress={this.onToggle.bind(this)}>
+                  <View style={{width: this.props.width, height: this.props.height}}>
+                    <View style={[styles.defaultParent, {position: 'absolute', top: 0, left: 0, width: this.props.width, height: this.props.height}]}>
+                      <View style={[styles.defaultTrack, this.props.trackStyle, {
+                        width: this.props.width, height: this.props.height,
+                        borderColor: (this.state.isOn) ? this.props.borderTrackOnColor : this.props.borderTrackOffColor,
+                        backgroundColor: (this.state.isOn) ? this.props.onColor : this.props.offColor
+                      }]}/>
+                    </View>
+                    <View style={[styles.defaultParent, {position: 'absolute', top: 0, left: 0, width: this.props.width, height: this.props.height }]}>
+                      <Animated.View style={{
+                        width: this.props.circleWidth,
+                        height: this.props.circleHeight,
+                        borderRadius: this.props.circleHeight / 2,
+                        position: 'absolute',
+                        left: 0,
+                        transform: [{translateX: this.state.animVal.interpolate({
+                          inputRange: [0, 1],
+                          outputRange: [0, this.props.width - this.props.circleWidth]
+                        })}],
+                        backgroundColor: (this.state.isOn) ? this.props.circleOnColor : this.props.circleOffColor
+                      }}/>
+                    </View>
+                    {this.props.offImageSource
+                      ? (
+                        <View style={[styles.defaultParent, {position: 'absolute', top: 0, left: 0, width: this.props.width, height: this.props.height }]}>
+                          <Animated.Image source={this.props.offImageSource} style={{
+                            opacity: this.state.animVal.interpolate({
+                              inputRange: [0, 1],
+                              outputRange: [1, 0]
+                            }),
+                            width: this.props.circleWidth,
+                            height: this.props.circleHeight,
+                            position: 'absolute',
+                            left: 0,
+                            transform: [{translateX: this.state.animVal.interpolate({
+                              inputRange: [0, 1],
+                              outputRange: [0, this.props.width - this.props.circleWidth]
+                            })}]
+                          }} />
+                        </View>
+                      ) : null
+                    }
+                    {this.props.onImageSource
+                      ? (
+                        <View style={[styles.defaultParent, {position: 'absolute', top: 0, left: 0, width: this.props.width, height: this.props.height }]}>
+                          <Animated.Image source={this.props.onImageSource} style={{
+                            opacity: this.state.animVal,
+                            width: this.props.circleWidth,
+                            height: this.props.circleHeight,
+                            position: 'absolute',
+                            left: 0,
+                            transform: [{translateX: this.state.animVal.interpolate({
+                              inputRange: [0, 1],
+                              outputRange: [0, this.props.width - this.props.circleWidth]
+                            })}]
+                          }} />
+                        </View>
+                      ) : null
+                    }
+                  </View>
                 </TouchableOpacity>
             </View>
         )
@@ -108,12 +167,24 @@ class ToggleSwitch extends React.Component{
 
 export default ToggleSwitch
 
-const styles = StyleSheet.create({
+const styles = {
     container: {
-        flexDirection: 'row',
-        alignItems: 'center'
+      flexDirection: 'row',
+      alignItems: 'center'
+    },
+    defaultParent: {
+      flexDirection: 'row',
+      alignItems: 'center',
+      height: 60
     },
     labelStyle:{
-        marginHorizontal: 10,
+      marginHorizontal: 10,
+    },
+    defaultTrack: {
+      top: 0,
+      justifyContent: 'center',
+      padding: 0,
+      margin: 0,
+      borderRadius: 20,
     }
-});
+};
